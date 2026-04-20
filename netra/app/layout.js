e7f823c378b26e7e44fd66f4719c7be7b803391d
ArgyaSabih <argy.sabih@gmail.com>
2026-04-20 19:57:59 +0700

    feat: add homepage hero section and navbar

--- netra/app/layout.js
+++ netra/app/layout.js
@@ -2,6 +2,7 @@ import "@/src/styles/globals.css";
 import {InterBold, InterLight, InterRegular, InterSemiBold, InterMedium} from "@/src/utils/helper/font";
 import {getMetaData} from "@/src/utils/helper/getMetaData";
 import AnimationProvider from "@/src/components/contexts/AnimationProvider";
+import Navbar from "@/src/components/navbar/Navbar";
 
 export const metadata = getMetaData({
   title: "NETRA",
@@ -14,6 +15,7 @@ export default function RootLayout({children}) {
       <body
         className={`${InterBold.variable} ${InterLight.variable} ${InterRegular.variable} ${InterSemiBold.variable} ${InterMedium.variable} antialiased`}
       >
+        <Navbar />
         <AnimationProvider>{children}</AnimationProvider>
       </body>
     </html>
--- netra/app/page.js
+++ netra/app/page.js
@@ -1,19 +1,5 @@
-import Image from "next/image";
+import Hero from "@/src/module/home/Hero";
 
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen font-sans bg-zinc-50 dark:bg-black">
-      <main className="flex flex-col items-center justify-center w-full max-w-3xl min-h-screen gap-8 px-16 py-32 bg-white dark:bg-black">
-        <Image src="/assets/favicon/favicon.svg" alt="Next.js logo" width={150} height={100} priority />
-        <div className="flex flex-col items-center gap-6 text-center">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            NETRA
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            A web-based and AI-powered SIEM with modern monitoring dashboard. Explore our{" "}
-          </p>
-        </div>
-      </main>
-    </div>
-  );
+  return <Hero />;
 }
